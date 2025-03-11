4ba16c421699e0be0e9c3f9d6dd22119df298f14
Augusto21 <camolesi2121@hotmail.com>
2025-03-10 22:19:19 -0300

    Inicial Commit

---
+++ src/test/java/com/augusto/desafio/sentry/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augusto.desafio.sentry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
