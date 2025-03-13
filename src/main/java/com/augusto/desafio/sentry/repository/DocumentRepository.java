1848248480030b56223fc482e6a780347124e301
Augusto21 <camolesi2121@hotmail.com>
2025-03-12 21:31:26 -0300

    fix: Correção de nome de tabela.

--- src/main/java/com/augusto/desafio/sentry/repository/DocumentRepository.java
+++ src/main/java/com/augusto/desafio/sentry/repository/DocumentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface DocumentRepository extends JpaRepository<Document, Long> {
   Optional<Document> findByName(String name);
 
-  @Query("SELECT COALESCE(SUM(d.tamanho), 0) FROM Documento d")
+  @Query("SELECT COALESCE(SUM(d.tamanho), 0) FROM document d")
   long somarTamanhoArquivos();
 
 }
